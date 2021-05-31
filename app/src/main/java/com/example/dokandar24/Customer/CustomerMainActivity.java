738dda76ae898365c2152962052d8b738446d50e
MdDoyalBabu <mddoyalbabucmt@gmail.com>
2021-05-31 17:58:14 +0600

    Dokandar24Com

---
+++ app/src/main/java/com/example/dokandar24/Customer/CustomerMainActivity.java
@@ -0,0 +1,15 @@
+package com.example.dokandar24.Customer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.dokandar24.R;
+
+public class CustomerMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_main);
+    }
+}
---
+++ app/src/main/java/com/example/dokandar24/Customer/CustomerRegistrationActivity.java
@@ -0,0 +1,16 @@
+package com.example.dokandar24.Customer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.dokandar24.R;
+
+public class CustomerRegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_registration);
+    }
+}
